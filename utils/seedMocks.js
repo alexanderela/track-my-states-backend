06d7a6e3924f508b3d3b03fe2a8e43edfb33084c
Alexander Ela <ela.alexander@gmail.com>
2019-06-29 18:56:48 -0600

    Add initial migration and seedMocks.js

---
+++ utils/seedMocks.js
@@ -0,0 +1,41 @@
+const mockUsers = [
+	{
+		'name': 'Alvin',
+		'email': 'alvin@chipmunks.com',
+		'password': 'password1',
+	},
+	{
+		'name': 'Simon',
+		'email': 'simon@chipmunks.com',
+		'password': 'password2',
+	},
+	{
+		'name': 'Theodore',
+		'email': 'theodore@chipmunks.com',
+		'password': 'password3',
+	}
+]
+
+const mockFavoriteStates = [
+	{
+		'state_name': 'Minnesota',
+		'number_of_stars': 0,
+		'been_to': false,
+		'lived_in': false,
+		'want_to_go': true,
+	},
+	{
+		'state_name': 'California',
+		'number_of_stars': 4,
+		'been_to': true,
+		'lived_in': true,
+		'want_to_go': false,
+	},
+	{
+		'state_name': 'Colorado',
+		'number_of_stars': 3,
+		'been_to': true,
+		'lived_in': true,
+		'want_to_go': false,
+	}	
+]
